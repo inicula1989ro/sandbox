be6083d82cfd666e43aba17753c77aba64828577
Aleksey Borisko <borisko.aleksey@gmail.com>
2026-04-18 15:07:47 +0300

    chore: add i18n + fontsource dependencies

--- packages/web/src/main.tsx
+++ packages/web/src/main.tsx
@@ -1,8 +1,18 @@
+import '@fontsource/playfair-display/400.css'
+import '@fontsource/playfair-display/600.css'
+import '@fontsource/playfair-display/700.css'
+import '@fontsource/cormorant-garamond/400-italic.css'
+import '@fontsource/cormorant-garamond/500-italic.css'
+import '@fontsource/inter/400.css'
+import '@fontsource/inter/500.css'
+import '@fontsource/inter/600.css'
+
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
 import { App } from '@/app/app'
 import { Providers } from '@/app/providers'
+import '@/lib/i18n'
 
 const root = document.getElementById('root')
 
