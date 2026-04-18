5ddc734aeaa7db1ac9a2549c05f6975f6388a891
Aleksey Borisko <borisko.aleksey@gmail.com>
2026-04-18 15:10:08 +0300

    feat(constants): add external links placeholders

---
+++ packages/web/src/constants/links.ts
@@ -0,0 +1,10 @@
+export const BOOKING_URL =
+  'https://n736120.alteg.io/company/139070/personal/menu?utm_source=ig&utm_medium=social&utm_content=link_in_bio&fbclid=PAb21jcARQUB1leHRuA2FlbQIxMQBzcnRjBmFwcF9pZA81NjcwNjczNDMzNTI0MjcAAaeZtJTKE0IqHIJuMNDmrDesldwE8yHsilw-0mFcYNS6LNt1gffszdMWRutu9g_aem_F651As61eVvNmPvwhcUoaw&utm_id=97760_v0_s00_e0_tv3_a1dennhb2qzpb9&o='
+
+export const PHONE_URL = '#'
+export const WHATSAPP_URL = '#'
+export const INSTAGRAM_URL = '#'
+export const FACEBOOK_URL = '#'
+
+export const MAP_EMBED_SRC =
+  'https://www.google.com/maps?q=str.+Nicolae+Starostenco+25,+Chisinau&output=embed'
